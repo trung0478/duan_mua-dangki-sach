445347bb5a896ab0a5014c37597feefc317cabf7
Nguyen-Thanh-Tung04 <thanhtung2432004@gmail.com>
2023-12-16 09:45:40 +0700

    taikhoan kh

--- user/view/taikhoan/taikhoankh.php
+++ user/view/taikhoan/taikhoankh.php
@@ -38,17 +38,14 @@
                         <div class="row">
                             <div class="col-lg-3 col-md-4">
                                 <div class="myaccount-tab-menu nav" role="tablist">
-                                    <a href="#dashboad" class="active" data-bs-toggle="tab"><i
-                                            class="fa fa-dashboard"></i>
-                                        Bảng điều khiển</a>
+                                    <a href="#account-info" class="active" data-bs-toggle="tab"><i
+                                            class="fa fa-user"></i> Chi tiết tài
+                                        khoản</a>
                                     <a href="#orders" data-bs-toggle="tab"><i class="fa fa-cart-arrow-down"></i>
                                         Đơn đặt hàng</a>
 
                                     <a href="#payment-method" data-bs-toggle="tab"><i class="fa fa-credit-card"></i>
                                         Phương thức thanh toán</a>
-
-                                    <a href="#account-info" data-bs-toggle="tab"><i class="fa fa-user"></i> Chi tiết tài
-                                        khoản</a>
                                     <a href="index.php?act=thoat"><i class="fa fa-sign-out"></i> Đăng xuất</a>
                                 </div>
                             </div>
@@ -57,20 +54,6 @@
                             <!-- My Account Tab Content Start -->
                             <div class="col-lg-9 col-md-8">
                                 <div class="tab-content" id="myaccountContent">
-                                    <!-- Single Tab Content Start -->
-                                    <div class="tab-pane fade show active" id="dashboad" role="tabpanel">
-                                        <div class="myaccount-content">
-                                            <h5>bảng điều khiển</h5>
-                                            <div class="welcome">
-                                                <p>Chào mừng bạn !</p>
-                                            </div>
-                                            <p class="mb-0">Từ bảng điều khiển tài khoản của bạn. bạn có thể dễ dàng
-                                                kiểm tra và xem các đơn đặt hàng gần đây của mình, quản lý địa chỉ giao
-                                                hàng và thanh toán cũng như chỉnh sửa chi tiết mật khẩu và tài khoản của
-                                                mình.</p>
-                                        </div>
-                                    </div>
-                                    <!-- Single Tab Content End -->
 
                                     <!-- Single Tab Content Start -->
                                     <div class="tab-pane fade" id="orders" role="tabpanel">
@@ -122,7 +105,7 @@
 
 
                                     <!-- Single Tab Content Start -->
-                                    <div class="tab-pane fade" id="payment-method" role="tabpanel">
+                                    <div class="tab-pane fade " id="payment-method" role="tabpanel">
                                         <div class="myaccount-content">
                                             <h5>Payment Method</h5>
                                             <p class="saved-message">You Can't Saved Your Payment Method yet.</p>
@@ -138,14 +121,43 @@
                                         extract($_SESSION['name']);
                                     }
                                     ?>
-                                    <div class="tab-pane fade" id="account-info" role="tabpanel">
+                                    <div class="tab-pane fade show active" id="account-info" role="tabpanel">
                                         <div class="myaccount-content">
                                             <h5>Chi tiết tài khoản</h5>
                                             <div class="account-details-form">
-                                                <form action="#">
-                                                    <div class="single-input-item">
-                                                        <label for="display-name" class="required">Tên tài khoản</label>
-                                                        <input type="text" id="display-name" value="<?= $ho_ten ?>" />
+                                                <form action="#" method="post">
+                                                    <div class="row">
+                                                        <div class="single-input-item col-lg-6">
+                                                            <label for="display-name" class="required">Tên tài
+                                                                khoản</label>
+                                                            <input type="text" id="display-name"
+                                                                value="<?= $ho_ten ?>" />
+                                                        </div>
+                                                        <div class="col-lg-6">
+                                                            <label>Avata</label>
+                                                            <div class="d-flex">
+                                                                <input style="width:50%;" type="file" name="hinh" id="">
+
+                                                                <?php
+                                                                if (isset($_SESSION['name'])) {
+                                                                    extract($_SESSION['name']);
+                                                                    if (!empty($anh)) {
+                                                                        $hinhpath = "upload/" . $anh;
+                                                                        if (is_file($hinhpath)) {
+                                                                            $hinh = "<img src='" . $hinhpath . "' style='height: 100px; width: 100px; border-radius: 50%;'>";
+                                                                        } else {
+                                                                            $hinh = "No photo";
+                                                                        }
+                                                                    } else {
+                                                                        $hinh = "<img src='upload/avata_null.jpg' alt='' ' style='height: 100px; width: 100px; border-radius: 50%;'>";
+                                                                    }
+                                                                ?>
+                                                                <?php echo $hinh ?>
+                                                                <?php
+                                                                }
+                                                                ?>
+                                                            </div>
+                                                        </div>
                                                     </div>
                                                     <div class="single-input-item">
                                                         <label for="display-name" class="required">Email</label>
@@ -153,38 +165,45 @@
                                                             readonly />
                                                     </div>
                                                     <div class="single-input-item">
-                                                        <label for="email" class="required">Email Addres</label>
-                                                        <input type="email" id="email" placeholder="Email Address" />
+                                                        <label for="email" class="required">Địa chỉ</label>
+                                                        <input type="email" id="email" value="<?= $diachi ?>" />
                                                     </div>
+                                                    <div class="single-input-item">
+                                                        <label for="email" class="required">Số điện thoại</label>
+                                                        <input type="email" id="email" value="<?= $so_dienthoai ?>" />
+                                                    </div>
+                                                    <div class="single-input-item">
+                                                        <button class="btn btn-sqr">Lưu</button>
+                                                    </div>
+                                                </form>
+                                                <form action="#" method="post">
                                                     <fieldset>
-                                                        <legend>Password change</legend>
+                                                        <legend>Thay đổi mật khẩu</legend>
                                                         <div class="single-input-item">
-                                                            <label for="current-pwd" class="required">Current
-                                                                Password</label>
-                                                            <input type="password" id="current-pwd"
-                                                                placeholder="Current Password" />
+                                                            <label for="current-pwd" class="required">Nhập mật khẩu
+                                                                cũ</label>
+                                                            <input type="password" id="current-pwd" />
                                                         </div>
                                                         <div class="row">
                                                             <div class="col-lg-6">
                                                                 <div class="single-input-item">
-                                                                    <label for="new-pwd" class="required">New
-                                                                        Password</label>
-                                                                    <input type="password" id="new-pwd"
-                                                                        placeholder="New Password" />
+                                                                    <label for="new-pwd" class="required">Mật khẩu
+                                                                        mới</label>
+                                                                    <input type="password" id="new-pwd" />
                                                                 </div>
                                                             </div>
                                                             <div class="col-lg-6">
                                                                 <div class="single-input-item">
-                                                                    <label for="confirm-pwd" class="required">Confirm
-                                                                        Password</label>
-                                                                    <input type="password" id="confirm-pwd"
-                                                                        placeholder="Confirm Password" />
+                                                                    <label for="confirm-pwd" class="required">Mật
+                                                                        khẩu
+                                                                        xác minh</label>
+                                                                    <input type="password" id="confirm-pwd" />
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </fieldset>
                                                     <div class="single-input-item">
-                                                        <button class="btn btn-sqr">Save Changes</button>
+                                                        <button class="btn btn-sqr">Lưu</button>
                                                     </div>
                                                 </form>
                                             </div>
